52f596cbc99fd60b4d322ff87da8a43037c757e6
joe <joe@DLTE6440>
2021-07-24 08:08:37 -0500

    comment on vsjoe

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Joe!" );
+        System.out.println("line3");
+        System.out.println(4);
     }
 }
